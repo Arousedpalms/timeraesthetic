c67f7116f65a20683bb6b4f0d92667c73f420906
Madd <149527002+Arousedpalms@users.noreply.github.com>
2023-11-08 17:16:51 -0500

    Update clock.js

--- docs/clock.js
+++ docs/clock.js
@@ -10,22 +10,67 @@ const customMinutes = document.getElementById('custom-minutes');
 let countdown;
 let timerSeconds = 0; // Timer set for 0 seconds initially
 
-// Define the timer functions and event listeners as before...
+// Timer function
+function timer(seconds) {
+  // Clears any existing timers
+  clearInterval(countdown);
 
-// Set the timer
+  const now = Date.now();
+  const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+}
+
+// Display time in HH:MM:SS format
+function displayTimeLeft(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
+  document.title = display;
+}
+
+// Start Timer
+startBtn.addEventListener('click', () => {
+  timer(timerSeconds);
+});
+
+// Stop Timer
+stopBtn.addEventListener('click', () => {
+  clearInterval(countdown);
+});
+
+// Reset Timer
+resetBtn.addEventListener('click', () => {
+  clearInterval(countdown);
+  timerDisplay.textContent = '00:00:00';
+  document.title = 'Timer Reset';
+  timerSeconds = 0;
+});
+
+// Set Timer
 setTimerBtn.addEventListener('click', () => {
-  const hours = parseInt(customHours.value) || 0;
-  const minutes = parseInt(customMinutes.value) || 0;
-  
-  if (hours >= 0 && minutes >= 0) {
+  const hours = parseInt(customHours.value, 10);
+  const minutes = parseInt(customMinutes.value, 10);
+
+  if (!isNaN(hours) && !isNaN(minutes) && hours >= 0 && minutes >= 0) {
     timerSeconds = (hours * 3600) + (minutes * 60);
     displayTimeLeft(timerSeconds);
   } else {
     alert('Please enter a valid number of hours and minutes.');
   }
 });
-
-// Rest of your JavaScript code for starting, stopping, and resetting the timer...
 
 // Initially update the display
 displayTimeLeft(timerSeconds);
