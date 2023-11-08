414685bf8997705ad197f5930488d3a1151d772d
Madd <149527002+Arousedpalms@users.noreply.github.com>
2023-11-08 17:11:14 -0500

    Update clock.js

--- docs/clock.js
+++ docs/clock.js
@@ -4,81 +4,28 @@ const startBtn = document.getElementById('start-btn');
 const stopBtn = document.getElementById('stop-btn');
 const resetBtn = document.getElementById('reset-btn');
 const setTimerBtn = document.getElementById('set-timer-btn');
-const customTimer = document.getElementById('custom-timer');
+const customHours = document.getElementById('custom-hours');
+const customMinutes = document.getElementById('custom-minutes');
 
 let countdown;
 let timerSeconds = 0; // Timer set for 0 seconds initially
 
-function timer(seconds) {
-  // Clears any existing timers
-  clearInterval(countdown);
-
-  const now = Date.now();
-  const then = now + seconds * 1000;
-  displayTimeLeft(seconds); // to display the time immediately, not after 1 second
-
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-    // Check if we should stop the timer
-    if (secondsLeft < 0) {
-      clearInterval(countdown);
-      playAlarm();
-      return;
-    }
-
-    // Display the remaining time
-    displayTimeLeft(secondsLeft);
-  }, 1000);
-}
-
-function displayTimeLeft(seconds) {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainderSeconds = seconds % 60;
-  const display = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
-  timerDisplay.textContent = display;
-  document.title = display; // Also updates the tab title with the timer
-}
-
-function playAlarm() {
-  // Ensure your HTML includes an audio element with the ID 'alarmSound'
-  const alarmSound = document.getElementById('alarmSound');
-  if (alarmSound) {
-    alarmSound.play();
-  }
-}
-
-// Start the timer
-startBtn.addEventListener('click', () => {
-  const minutes = parseInt(customTimer.value);
-  const seconds = isNaN(minutes) ? timerSeconds : minutes * 60;
-  timer(seconds);
-});
-
-// Stop the timer
-stopBtn.addEventListener('click', () => {
-  clearInterval(countdown);
-});
-
-// Reset the timer
-resetBtn.addEventListener('click', () => {
-  clearInterval(countdown);
-  timerDisplay.textContent = '00:00:00';
-  document.title = 'Timer Reset';
-  timerSeconds = 0; // Reset the timer seconds
-});
+// Define the timer functions and event listeners as before...
 
 // Set the timer
 setTimerBtn.addEventListener('click', () => {
-  const minutes = parseInt(customTimer.value);
-  if (!isNaN(minutes) && minutes >= 0) {
-    timerSeconds = minutes * 60;
+  const hours = parseInt(customHours.value) || 0;
+  const minutes = parseInt(customMinutes.value) || 0;
+  
+  if (hours >= 0 && minutes >= 0) {
+    timerSeconds = (hours * 3600) + (minutes * 60);
     displayTimeLeft(timerSeconds);
   } else {
-    alert('Please enter a valid number of minutes.');
+    alert('Please enter a valid number of hours and minutes.');
   }
 });
+
+// Rest of your JavaScript code for starting, stopping, and resetting the timer...
 
 // Initially update the display
 displayTimeLeft(timerSeconds);
